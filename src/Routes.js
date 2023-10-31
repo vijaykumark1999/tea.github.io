06ef34a050b9e4728e4ed7a27b8e5e475e8bafd6
VIJAY KUMAR K <104831463+vijaykumark1999@users.noreply.github.com>
2023-11-01 00:58:20 +0530

    Update Routes.js

--- src/Routes.js
+++ src/Routes.js
@@ -16,7 +16,7 @@ const router = createBrowserRouter(
                 },
                 {
                     path: "/about",
-                    element: <About />
+                    element: <About />,
                     index: true
                 }
             ]
